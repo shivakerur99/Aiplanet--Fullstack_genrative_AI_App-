7ead033b7fcd4cd666476b41b606cbbcb9d028e3
cruisershiva <shivakerur99@gmail.com>
2024-03-12 03:36:20 +0530

    tenth commit

--- aiplanet-app/src/components/Api.js
+++ aiplanet-app/src/components/Api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const Api=axios.create({
-    baseURL: "http://localhost:8000", //for localhost connection with FastAPI backend 
-    // baseURL:"https://aiplanet-fullstack-genrative-ai-app.onrender.com"   //connection with deployed backend deployed FastAPI app 
+    // baseURL: "http://localhost:8000", //for localhost connection with FastAPI backend 
+    baseURL:"https://aiplanet-fullstack-genrative-ai-app.onrender.com"   //connection with deployed backend deployed FastAPI app 
 })
 
 export default Api
