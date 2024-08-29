0ed030ded6f2a00a7ac51e3593e255eddaed5e47
cruisershiva <shivakerur99@gmail.com>
2024-08-30 00:37:37 +0530

    added all files

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -106,16 +106,16 @@ async def process_data(data: DataInput):
     user_input = data.userInput
     
     # Load required models and components from Langchain library
-    os.environ['HUGGINGFACEHUB_API_TOKEN'] ="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" 
+    os.environ['HUGGINGFACEHUB_API_TOKEN'] ="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" 
     dom = [LangchainDocument(page_content=response_data, metadata={"source": "local"})]
 
-    text_spliter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    text_spliter = CharacterTextSplitter(chunk_size=3000, chunk_overlap=0)
     docs = text_spliter.split_documents(dom)
 
     embeddings = HuggingFaceEmbeddings()
     db = FAISS.from_documents(docs, embeddings)
     llm = HuggingFaceEndpoint(
-        repo_id="google/flan-t5-xxl",
+        repo_id="meta/llama-2-7b-chat",
         temperature=0.8,
     )
 
